22efa85fa254f80d37eb258fd9ee676879292578
Simon Van Leuven <simon.van.leuven@qite.be>
2019-01-06 16:54:05 +0100

    Additional components

---
+++ MasterTrainer.Web/src/app/modules/main/components/instructions/instructions.component.ts
@@ -0,0 +1,13 @@
+﻿import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-instructions',
+    templateUrl: './instructions.component.html',
+    styles: [':host { width: 100%; }']
+})
+export class InstructionsComponent implements OnInit {
+
+    constructor() { }
+
+    public ngOnInit = () => { }
+}
--- MasterTrainer.Web/src/app/modules/main/main.routes.ts
+++ MasterTrainer.Web/src/app/modules/main/main.routes.ts
@@ -1,7 +1,11 @@
 ﻿import { Routes } from '@angular/router';
 
 import { HomeComponent } from './components/home/home.component';
+import { InstructionsComponent } from './components/instructions/instructions.component';
+import { AboutComponent } from './components/about/about.component';
 
 export const mainRoutes: Routes = [
-    { path: 'home', component: HomeComponent }
+    { path: 'home', component: HomeComponent },
+    { path: 'instructions', component: InstructionsComponent },
+    { path: 'about', component: AboutComponent }
 ];
